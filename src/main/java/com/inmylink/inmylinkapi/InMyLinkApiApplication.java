a36f49e53a3ee2fcb39cd81e4c67c83d5bb110f3
jangmyeongji <2hundred2ne2@gmail.com>
2024-08-22 13:17:58 +0900

    환경세팅

---
+++ src/main/java/com/inmylink/inmylinkapi/InMyLinkApiApplication.java
@@ -0,0 +1,13 @@
+package com.inmylink.inmylinkapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InMyLinkApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InMyLinkApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/inmylink/inmylinkapi/InMyLinkApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inmylink.inmylinkapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InMyLinkApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
